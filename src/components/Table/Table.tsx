c4be5efdba09f416f79b1e2a6d75f1959e2fa849
bospur <bospobob@yandex.ru>
2024-11-06 14:41:32 +0500

    Управление состоянием таблицы

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,24 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { Table as MuiTable } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Input,
+  MenuItem,
+  Table as MuiTable,
+  Select,
+  TextField,
+} from "@material-ui/core";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ChangeEvent, useState } from "react";
 
 const useStyles = makeStyles({
   table: {
@@ -17,107 +29,243 @@ const useStyles = makeStyles({
     color: "#8E8E93",
     fontSize: 14,
   },
+  dialog: {
+    minWidth: 450,
+  },
+  select: {
+    width: "100%",
+    marginTop: 25,
+    marginBottom: 35,
+    height: 50,
+  },
+  dialogContent: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+  },
 });
 
-const createData = (
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) => {
-  return { name, calories, fat, carbs, protein };
-};
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 type Column = {
-  id: number;
+  id: string;
   name: string;
   type: string;
 };
 
+type Row = Record<string, string | number>;
+
 const INITIAL_COLUMNS = [
   {
-    id: 1,
+    id: uuidv4(),
     name: "Колонка 1",
     type: "string",
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: "Колонка 2",
     type: "number",
   },
 ];
 
 const INITIAL_ROWS = [
-  {
-    id: 1,
-    column1: "sssss",
-    column2: 5,
-  },
-  {
-    id: 2,
-    column1: "фффф",
-    column2: 10,
-  },
+  // {
+  //   id: uuidv4(),
+  //   column1: "sssss",
+  //   column2: 5,
+  // },
+  // {
+  //   id: uuidv4(),
+  //   column1: "фффф",
+  //   column2: 10,
+  // },
 ];
 
 const getColumnsDecs = (column: Column) =>
   column.type === "string" ? "" : "(%)";
 
 export const Table = () => {
-  const [columns, setColumns] = useState<Column[]>(() => INITIAL_COLUMNS);
-  const [rows, setRows] = useState(() => INITIAL_ROWS);
+  const [columns, setColumns] = useState<Column[]>([]);
+  const [rows, setRows] = useState<Row[]>([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [dialogState, setDialogSate] = useState({
+    name: "",
+    type: "",
+  });
   const classes = useStyles();
 
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+  const handleChangeColumnSelect = () => setDialogSate;
+  const handleAddColumn = () => {
+    const newColumns = [
+      ...columns,
+      {
+        id: uuidv4(),
+        ...dialogState,
+      },
+    ];
+
+    if (rows.length) {
+      const newRows = [
+        ...rows.map((row) => {
+          let newRow: Row = {};
+
+          newColumns.forEach((e) => {
+            newRow[e.name] = "";
+          });
+
+          return { id: uuidv4(), ...newRow, ...row };
+        }),
+      ];
+
+      setRows(newRows);
+    }
+
+    const newRows = [];
+    let newRow: Row = {
+      id: uuidv4(),
+    };
+
+    newColumns.forEach((e) => {
+      newRow[e.name] = "";
+    });
+
+    newRows.push(newRow);
+
+    setColumns(newColumns);
+    setRows(newRows);
+    setIsOpen(false);
+    setDialogSate({
+      name: "",
+      type: "",
+    });
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <MuiTable
-        className={classes.table}
-        size="small"
-        aria-label="a dense table"
-      >
+    <TableContainer component={Paper} className={classes.table}>
+      <div>
+        <Button onClick={handleOpen}>Добавить колонку</Button>
+        <Button onClick={handleEdit}>
+          {isEdit ? "сохранить изменения" : "Редактировать содержимое"}
+        </Button>
+      </div>
+      <MuiTable size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
             {columns.map((column) => {
               return (
-                <TableCell className={classes.columnHeader} align="left">
-                  {column.name}
-                  {getColumnsDecs(column)}
+                <TableCell
+                  className={classes.columnHeader}
+                  align="left"
+                  key={column.id}
+                >
+                  {isEdit ? (
+                    <Input
+                      value={column.name}
+                      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        setColumns(
+                          columns.map((editColum) => {
+                            if (editColum.id === column.id) {
+                              editColum.name = e.target.value;
+                            }
+
+                            return editColum;
+                          })
+                        )
+                      }
+                    />
+                  ) : (
+                    `${column.name}${getColumnsDecs(column)}`
+                  )}
                 </TableCell>
               );
             })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {/* {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))} */}
           {rows.map((row) => {
             return (
               <TableRow key={row.id}>
-                <TableCell align="left">{row.column1}</TableCell>
-                <TableCell align="left">{row.column2}</TableCell>
+                {Object.keys(row)
+                  .slice(1)
+                  .map((key) => {
+                    return (
+                      <TableCell align="left" key={key}>
+                        {isEdit ? (
+                          <Input
+                            value={row[key as keyof typeof row]}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                              setRows(
+                                rows.map((editRow) => {
+                                  if (row.id === editRow.id) {
+                                    editRow[key] = e.target.value;
+                                  }
+
+                                  return editRow;
+                                })
+                              );
+                            }}
+                          />
+                        ) : (
+                          row[key as keyof typeof row]
+                        )}
+                      </TableCell>
+                    );
+                  })}
               </TableRow>
             );
           })}
         </TableBody>
       </MuiTable>
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" className={classes.dialog}>
+          Добавление колонки
+        </DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <TextField
+            id="outlined-basic"
+            label="Заголовок"
+            variant="outlined"
+            value={dialogState.name}
+            onChange={(e) =>
+              setDialogSate({ ...dialogState, name: e.target.value })
+            }
+          />
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-simple-select"
+            label="aaa"
+            value={dialogState.type}
+            onChange={(e) =>
+              setDialogSate({ ...dialogState, type: String(e.target.value) })
+            }
+            className={classes.select}
+          >
+            <MenuItem value="string">Строка</MenuItem>
+            <MenuItem value="number">Проценты</MenuItem>
+          </Select>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Закрыть</Button>
+          <Button
+            onClick={handleAddColumn}
+            autoFocus
+            disabled={Boolean(
+              !dialogState.name.length && !dialogState.type.length
+            )}
+          >
+            Добавить
+          </Button>
+        </DialogActions>
+      </Dialog>
     </TableContainer>
   );
 };
